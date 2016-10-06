c7df9617ab01b9737c8bc16c80ba4cdc4224e422
kkllPreciel <zie35-gymiko@jcom.zaq.ne.jp>
2016-10-07 00:39:29 +0900

    コメントを追加

--- HelloWindow/HelloWindow/HelloWindow.cpp
+++ HelloWindow/HelloWindow/HelloWindow.cpp
@@ -33,9 +33,9 @@ HelloWindow::~HelloWindow()
  */
 void HelloWindow::OnInit()
 {
-	// パイプラインの作成
 #if defined(_DEBUG)
 	// Enable the D3D12 debug layer.
+	// デバッグレイヤーを有効化
 	{
 		ComPtr<ID3D12Debug> debugController;
 		if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController))))
@@ -47,20 +47,48 @@ void HelloWindow::OnInit()
 
 	ComPtr<IDXGIFactory4> factory;
 
+	// ファクトリの作成
 	if (FAILED(CreateDXGIFactory1(IID_PPV_ARGS(&factory))))
 		throw;
 
-	if (FAILED(D3D12CreateDevice(nullptr, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&m_device))))
-		throw;
+	// デバイスの生成
+	{
+		if (FAILED(D3D12CreateDevice(nullptr, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&m_device))))
+			throw;
+	}
 
-	// Describe and create the command queue.
-	D3D12_COMMAND_QUEUE_DESC queueDesc = {};
-	queueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
-	queueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+	// コマンドキューの生成
+	{
+		// Describe and create the command queue.
+		D3D12_COMMAND_QUEUE_DESC queueDesc = {};
+		queueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+		queueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
 
-	if (FAILED(m_device->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&m_commandQueue))))
-		throw;
+		if (FAILED(m_device->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&m_commandQueue))))
+			throw;
+	}
 
+	// コマンドアロケータの生成
+	{
+		if (FAILED(m_device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&m_commandAllocator))))
+			throw;
+	}
+
+	// コマンドリストの生成
+	{
+		// Create the command list.
+		if (FAILED(m_device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, m_commandAllocator.Get(), nullptr, IID_PPV_ARGS(&m_commandList))))
+			throw;
+
+		// Command lists are created in the recording state, but there is nothing
+		// to record yet. The main loop expects it to be closed, so close it now.
+		// コマンドリストを一旦閉じる
+		if (FAILED(m_commandList->Close()))
+			throw;
+	}
+
+	// スワップチェインの生成
+	// ウインドウへの表示やダブルバッファリングなどを行うクラス。
 	// Describe and create the swap chain.
 	DXGI_SWAP_CHAIN_DESC1 swapChainDesc = {};
 	swapChainDesc.BufferCount = FrameCount;
@@ -92,19 +120,22 @@ void HelloWindow::OnInit()
 
 	m_frameIndex = m_swapChain->GetCurrentBackBufferIndex();
 
+	// スワップチェインをRenderTargetとして使用するためのDescriptorHeapを作成
 	// Create descriptor heaps.
 	{
 		// Describe and create a render target view (RTV) descriptor heap.
 		D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc = {};
-		rtvHeapDesc.NumDescriptors = FrameCount;
-		rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
-		rtvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+		rtvHeapDesc.NumDescriptors = FrameCount;				// ダブルバッファリング(フレーム、バック)をするので2つ作成
+		rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;		// 種別はレンダーターゲット
+		rtvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;	// シェーダからアクセスしない
 		if (FAILED(m_device->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&m_rtvHeap))))
 			throw;
 
+		// レンダーターゲット分のDescriptorのサイズを取得する
 		m_rtvDescriptorSize = m_device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
 	}
 
+	// スワップチェインのバッファを先に作成したDescriptorHeapに登録する
 	// Create frame resources.
 	{
 		D3D12_CPU_DESCRIPTOR_HANDLE rtvHandle(m_rtvHeap->GetCPUDescriptorHandleForHeapStart());
@@ -119,20 +150,10 @@ void HelloWindow::OnInit()
 		}
 	}
 
-	if (FAILED(m_device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&m_commandAllocator))))
-		throw;
-
 	// アセットの読み込み
 
-	// Create the command list.
-	if (FAILED(m_device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, m_commandAllocator.Get(), nullptr, IID_PPV_ARGS(&m_commandList))))
-		throw;
-
-	// Command lists are created in the recording state, but there is nothing
-	// to record yet. The main loop expects it to be closed, so close it now.
-	if (FAILED(m_commandList->Close()))
-		throw;
-
+	// Directx12では描画の終了待ちを自動で行わない(同期が取れず画面がおかしくなる)
+	// そのため同期を取るためのオブジェクト(フェンス)を作成する
 	// Create synchronization objects.
 	{
 		if (FAILED(m_device->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&m_fence))))
@@ -166,12 +187,14 @@ void HelloWindow::OnRender()
 	// Command list allocators can only be reset when the associated 
 	// command lists have finished execution on the GPU; apps should use 
 	// fences to determine GPU execution progress.
+	// コマンドアロケータをリセットする(バッファを全て開放する)
 	if (FAILED(m_commandAllocator->Reset()))
 		throw;
 
 	// However, when ExecuteCommandList() is called on a particular command 
 	// list, that command list can then be reset at any time and must be before 
 	// re-recording.
+	// コマンドリストをリセットする
 	if (FAILED(m_commandList->Reset(m_commandAllocator.Get(), m_pipelineState.Get())))
 		throw;
 
@@ -190,6 +213,7 @@ void HelloWindow::OnRender()
 	rtvHandle.ptr = m_rtvHeap->GetCPUDescriptorHandleForHeapStart().ptr + m_frameIndex * m_rtvDescriptorSize;
 
 	// Record commands.
+	// コマンドを積む(レンダーターゲットを指定した色でクリアする)
 	const float clearColor[] = { 0.0f, 0.2f, 0.4f, 1.0f };
 	m_commandList->ClearRenderTargetView(rtvHandle, clearColor, 0, nullptr);
 
@@ -204,10 +228,12 @@ void HelloWindow::OnRender()
 	// Indicate that the back buffer will now be used to present.
 	m_commandList->ResourceBarrier(1, &barrier2);
 
+	// コマンドを積み終わったので閉じる
 	if (FAILED(m_commandList->Close()))
 		throw;
 
 	// Execute the command list.
+	// コマンドリストをコマンドキューに実行させる(コマンドキューに移している?)
 	ID3D12CommandList* ppCommandLists[] = { m_commandList.Get() };
 	m_commandQueue->ExecuteCommandLists(_countof(ppCommandLists), ppCommandLists);
 
@@ -221,16 +247,22 @@ void HelloWindow::OnRender()
 	// maximize GPU utilization.
 
 	// Signal and increment the fence value.
+	// 現在のFence値がコマンド終了後にFenceに書き込まれるようにする
 	const UINT64 fence = m_fenceValue;
 	if (FAILED(m_commandQueue->Signal(m_fence.Get(), fence)))
 		throw;
 	m_fenceValue++;
 
 	// Wait until the previous frame is finished.
+	// まだコマンドキューが終了していないことを確認する
+	// ここまででコマンドキューが終了してしまうとイベントが一切発火されなくなるのでチェックしている
 	if (m_fence->GetCompletedValue() < fence)
 	{
+		// このFenceにおいて、fvalue の値になったらイベントを発火させる
 		if (FAILED(m_fence->SetEventOnCompletion(fence, m_fenceEvent)))
 			throw;
+
+		// イベントが発火するまで待つ
 		WaitForSingleObject(m_fenceEvent, INFINITE);
 	}
 

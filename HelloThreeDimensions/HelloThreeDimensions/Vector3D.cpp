26284ef712ea3df08fb1ee9c28c6860485a88c0c
kkllPreciel <zie35-gymiko@jcom.zaq.ne.jp>
2017-01-17 23:21:43 +0900

    数学関数を更新
    3Dベクトルの構造体を更新

--- HelloThreeDimensions/HelloThreeDimensions/Vector3D.cpp
+++ HelloThreeDimensions/HelloThreeDimensions/Vector3D.cpp
@@ -1,6 +1,6 @@
 /**
- *	@file		Vector2D.cpp
- *	@brief		2次元ベクトルに関する処理行うプログラムソース
+ *	@file		Vector3D.cpp
+ *	@brief		3次元ベクトルに関する処理行うプログラムソース
  *	@author		kkllPreciel
  *	@date		2017/01/17
  *	@version	1.0
@@ -12,6 +12,7 @@
 #include <limits>
 #include <crtdbg.h>
 #include "Vector2D.h"
+#include "Vector3D.h"
 
 namespace Sein
 {
@@ -20,36 +21,47 @@ namespace Sein
 		/**
 		 *	@brief	コンストラクタ
 		 */
-		Vector2D::Vector2D()
+		Vector3D::Vector3D()
 		{
-		
+
 		}
 		
 		/**
 		 *	@brief	引数付きコンストラクタ
 		 *	@param	x:x成分
 		 *	@param	y:y成分
+		 *	@param	z:z成分
 		 */
-		Vector2D::Vector2D(float x, float y) : x(x), y(y)
+		Vector3D::Vector3D(float x, float y, float z) : x(x), y(y), z(z)
 		{
+
+		}
 		
+		/**
+		 *	@brief	引数付きコンストラクタ
+		 *	@param	vector:2次元ベクトル
+		 *	@param	z:z成分
+		 */
+		Vector3D::Vector3D(const Vector2D& vector, float z) : x(vector.x), y(vector.y), z(z)
+		{
+
 		}
 		
 		/**
 		 *	@brief	コピーコンストラクタ
 		 *	@param	vector:コピー元のインスタンス
 		 */
-		Vector2D::Vector2D(const Vector2D& vector) : x(vector.x), y(vector.y)
+		Vector3D::Vector3D(const Vector3D& vector) : x(vector.x), y(vector.y), z(vector.z)
 		{
-		
+
 		}
 		
 		/**
 		 *	@brief	デストラクタ
 		 */
-		Vector2D::~Vector2D()
+		Vector3D::~Vector3D()
 		{
-		
+
 		}
 		
 		/**
@@ -57,65 +69,70 @@ namespace Sein
 		 *	@param	vector:代入する値
 		 *	@return	代入結果
 		 */
-		Vector2D& Vector2D::operator = (const Vector2D& vector)
+		Vector3D& Vector3D::operator = (const Vector3D& vector)
 		{
 			x = vector.x;
 			y = vector.y;
-		
+			z = vector.z;
+
 			return *this;
 		}
-		
+
 		/**
 		 *	@brief	加算代入演算子
 		 *	@param	vector:加算する値
 		 *	@return	加算代入結果
 		 */
-		Vector2D& Vector2D::operator += (const Vector2D& vector)
+		Vector3D& Vector3D::operator += (const Vector3D& vector)
 		{
 			x += vector.x;
 			y += vector.y;
-		
+			z += vector.z;
+
 			return *this;
 		}
-		
+
 		/**
 		 *	@brief	減算代入演算子
 		 *	@param	vector:減算する値
 		 *	@return	減算代入結果
 		 */
-		Vector2D& Vector2D::operator -= (const Vector2D& vector)
+		Vector3D& Vector3D::operator -= (const Vector3D& vector)
 		{
 			x -= vector.x;
 			y -= vector.y;
-		
+			z -= vector.z;
+
 			return *this;
 		}
-		
+
 		/**
 		 *	@brief	乗算代入演算子
 		 *	@param	multiplier:乗算する値
 		 *	@return	乗算代入結果
 		 */
-		Vector2D& Vector2D::operator *= (float multiplier)
+		Vector3D& Vector3D::operator *= (float multiplier)
 		{
 			x *= multiplier;
 			y *= multiplier;
-		
+			z *= multiplier;
+
 			return *this;
 		}
-		
+
 		/**
 		 *	@brief	除算代入演算子
 		 *	@param	divisor:除算する値
 		 *	@return	除算代入結果
 		 */
-		Vector2D& Vector2D::operator /= (float divisor)
+		Vector3D& Vector3D::operator /= (float divisor)
 		{
 #ifdef _DEBUG
-			_ASSERTE(std::numeric_limits<float>::epsilon() <= divisor && "division by zero.(Vector2D::operator/=)");
+			_ASSERTE(std::numeric_limits<float>::epsilon() <= divisor && "division by zero.(Vector3D::operator/=)");
 #endif;
 			x /= divisor;
 			y /= divisor;
+			z /= divisor;
 
 			return *this;
 		}
@@ -125,9 +142,9 @@ namespace Sein
 		 *	@param	vector:加算する値
 		 *	@retun	加算結果
 		 */
-		Vector2D Vector2D::operator + (const Vector2D& vector) const
+		Vector3D Vector3D::operator + (const Vector3D& vector) const
 		{
-			return Vector2D(x + vector.x, y + vector.y);
+			return Vector3D(x + vector.x, y + vector.y, z + vector.z);
 		}
 
 		/**
@@ -135,9 +152,9 @@ namespace Sein
 		 *	@param	vector:減算する値
 		 *	@return	減算結果
 		 */
-		Vector2D Vector2D::operator - (const Vector2D& vector) const
+		Vector3D Vector3D::operator - (const Vector3D& vector) const
 		{
-			return Vector2D(x - vector.x, y - vector.y);
+			return Vector3D(x - vector.x, y - vector.y, z - vector.z);
 		}
 
 		/**
@@ -145,9 +162,9 @@ namespace Sein
 		 *	@param	multiplier:乗算する値
 		 *	@return	乗算結果
 		 */
-		Vector2D Vector2D::operator * (float multiplier) const
+		Vector3D Vector3D::operator * (float multiplier) const
 		{
-			return Vector2D(x * multiplier, y * multiplier);
+			return Vector3D(x * multiplier, y * multiplier, z * multiplier);
 		}
 
 		/**
@@ -155,12 +172,12 @@ namespace Sein
 		 *	@param	divisor:除算する値
 		 *	@return	除算結果
 		 */
-		Vector2D Vector2D::operator / (float divisor) const
+		Vector3D Vector3D::operator / (float divisor) const
 		{
 #ifdef _DEBUG
-			_ASSERTE(std::numeric_limits<float>::epsilon() <= divisor && "division by zero.(Vector2D::operator/)");
+			_ASSERTE(std::numeric_limits<float>::epsilon() <= divisor && "division by zero.(Vector3D::operator/)");
 #endif;
-			return Vector2D(x / divisor, y / divisor);
+			return Vector3D(x / divisor, y / divisor, z / divisor);
 		}
 
 		/**
@@ -168,11 +185,12 @@ namespace Sein
 		 *	@param	vector:比較する値
 		 *	@return	bool。等価ならtrue, 非等価ならfalse
 		 */
-		bool Vector2D::operator == (const Vector2D& vector) const
+		bool Vector3D::operator == (const Vector3D& vector) const
 		{
 			// 差分がepsilonより小さい
 			return fabsf(x - vector.x) <= std::numeric_limits<float>::epsilon()
-				&& fabsf(y - vector.y) <= std::numeric_limits<float>::epsilon();
+				&& fabsf(y - vector.y) <= std::numeric_limits<float>::epsilon()
+				&& fabsf(z - vector.z) <= std::numeric_limits<float>::epsilon();
 		}
 
 		/**
@@ -180,59 +198,64 @@ namespace Sein
 		 *	@param	vector:比較する値
 		 *	@return	bool。非等価ならtrue,等価ならfalse
 		 */
-		bool Vector2D::operator != (const Vector2D& vector) const
+		bool Vector3D::operator != (const Vector3D& vector) const
 		{
 			// 差分がepsilonより大きい
 			return std::numeric_limits<float>::epsilon() < fabsf(x - vector.x)
-				|| std::numeric_limits<float>::epsilon() < fabsf(y - vector.y);
+				|| std::numeric_limits<float>::epsilon() < fabsf(y - vector.y)
+				|| std::numeric_limits<float>::epsilon() < fabsf(z - vector.z);
 		}
 
 		/**
 		 *	@brief	長さを取得する
 		 *	@return	長さ
 		 */
-		float Vector2D::Length() const
+		float Vector3D::Length() const
 		{
-			return sqrtf(x * x + y * y);
+			return sqrtf(x * x + y * y + z * z);
 		}
-
+		
 		/**
 		 *	@brief	正規化を行う
 		 *	@return	正規化結果
 		 */
-		Vector2D& Vector2D::Normalize()
+		Vector3D& Vector3D::Normalize()
 		{
 			float fLength = Length();
 #ifdef _DEBUG
-			_ASSERTE(std::numeric_limits<float>::epsilon() <= fLength && "division by zero.(Vector2D::Normalize)");				
+			_ASSERTE(std::numeric_limits<float>::epsilon() <= fLength && "division by zero.(Vector3D::Normalize)");
 #endif;
 			x /= fLength;
 			y /= fLength;
 
 			return *this;
 		}
-
+		
 		/**
 		 *	@brief	内積を行う
 		 *	@param	vector1:入力ベクトル
 		 *	@param	vector2:入力ベクトル
 		 *	@return	内積結果
 		 */
-		float Vector2D::DotProduct(const Vector2D& vector1, const Vector2D& vector2)
+		float Vector3D::DotProduct(const Vector3D& vector1, const Vector3D& vector2)
 		{
 			// 内積は各要素の掛け算の和
-			return vector1.x * vector2.x + vector1.y * vector2.y;
+			return vector1.x * vector2.x + vector1.y * vector2.y + vector1.z * vector2.z;
 		}
-
+		
 		/**
 		 *	@brief	外積を行う
 		 *	@param	vector1:入力ベクトル
 		 *	@param	vector2:入力ベクトル
 		 *	@return	外積結果
 		 */
-		float Vector2D::CrossProduct(const Vector2D& vector1, const Vector2D& vector2)
+		Vector3D Vector3D::CrossProduct(const Vector3D& vector1, const Vector3D& vector2)
 		{
-			return vector1.x * vector2.y - vector1.y * vector2.x;
+			return Vector3D(
+				vector1.y * vector2.z - vector1.z * vector2.y,
+				vector1.z * vector2.x - vector1.x * vector2.z,
+				vector1.x * vector2.y - vector1.y * vector2.x
+			);
 		}
 	}
 }
--- HelloThreeDimensions/HelloThreeDimensions/Vector3D.h
+++ HelloThreeDimensions/HelloThreeDimensions/Vector3D.h
@@ -1,1 +1,171 @@
+/**
+ *	@file		Vector3D.h
+ *	@brief		3次元ベクトルに関するヘッダファイル
+ *	@author		kkllPreciel
+ *	@date		2017/01/17
+ *	@version	1.0
+ *  @copyright	(c) 2017, kkllPreciel
+ */
+
+// 2重includeの防止
 #pragma once
+
+namespace Sein
+{
+	namespace Math
+	{
+		// 構造体の宣言
+		struct Vector2D;
+
+		/**
+		 *	@brief	3次元ベクトル構造体
+		 */
+		struct Vector3D
+		{
+			/**
+			 *	@brief	コンストラクタ
+			 */
+			Vector3D();
+
+			/**
+			 *	@brief	引数付きコンストラクタ
+			 *	@param	x:x成分
+			 *	@param	y:y成分
+			 *	@param	z:z成分
+			 */
+			Vector3D(float x, float y, float z);
+
+			/**
+			 *	@brief	引数付きコンストラクタ
+			 *	@param	vector:2次元ベクトル
+			 *	@param	z:z成分
+			 */
+			Vector3D(const Vector2D& vector, float z);
+
+			/**
+			 *	@brief	コピーコンストラクタ
+			 *	@param	vector:コピー元のインスタンス
+			 */
+			Vector3D(const Vector3D& vector);
+
+			/**
+			 *	@brief	デストラクタ
+			 */
+			~Vector3D();
+
+			/**
+			 *	@brief	代入演算子
+			 *	@param	vector:代入する値
+			 *	@return	代入結果
+			 */
+			Vector3D& operator = (const Vector3D& vector);
+
+			/**
+			 *	@brief	加算代入演算子
+			 *	@param	vector:加算する値
+			 *	@return	加算代入結果
+			 */
+			Vector3D& operator += (const Vector3D& vector);
+
+			/**
+			 *	@brief	減算代入演算子
+			 *	@param	vector:減算する値
+			 *	@return	減算代入結果
+			 */
+			Vector3D& operator -= (const Vector3D& vector);
+
+			/**
+			 *	@brief	乗算代入演算子
+			 *	@param	multiplier:乗算する値
+			 *	@return	乗算代入結果
+			 */
+			Vector3D& operator *= (float multiplier);
+
+			/**
+			 *	@brief	除算代入演算子
+			 *	@param	divisor:除算する値
+			 *	@return	除算代入結果
+			 */
+			Vector3D& operator /= (float divisor);
+
+			/**
+			 *	@brief	加算演算子
+			 *	@param	vector:加算する値
+			 *	@retun	加算結果
+			 */
+			Vector3D operator + (const Vector3D& vector) const;
+
+			/**
+			 *	@brief	減算演算子
+			 *	@param	vector:減算する値
+			 *	@return	減算結果
+			 */
+			Vector3D operator - (const Vector3D& vector) const;
+
+			/**
+			 *	@brief	乗算演算子
+			 *	@param	multiplier:乗算する値
+			 *	@return	乗算結果
+			 */
+			Vector3D operator * (float multiplier) const;
+
+			/**
+			 *	@brief	除算演算子
+			 *	@param	divisor:除算する値
+			 *	@return	除算結果
+			 */
+			Vector3D operator / (float divisor) const;
+
+			/**
+			 *	@brief	等価比較演算子
+			 *	@param	vector:比較する値
+			 *	@return	bool。等価ならtrue, 非等価ならfalse
+			 */
+			bool operator == (const Vector3D& vector) const;
+
+			/**
+			 *	@brief	非等価比較演算子
+			 *	@param	vector:比較する値
+			 *	@return	bool。非等価ならtrue,等価ならfalse
+			 */
+			bool operator != (const Vector3D& vector) const;
+
+			/**
+			 *	@brief	長さを取得する
+			 *	@return	長さ
+			 */
+			float Length() const;
+
+			/**
+			 *	@brief	正規化を行う
+			 *	@return	正規化結果
+			 */
+			Vector3D& Normalize();
+
+			/**
+			 *	@brief	内積を行う
+			 *	@param	vector1:入力ベクトル
+			 *	@param	vector2:入力ベクトル
+			 *	@return	内積結果
+			 */
+			static float DotProduct(const Vector3D& vector1, const Vector3D& vector2);
+
+			/**
+			 *	@brief	外積を行う
+			 *	@param	vector1:入力ベクトル
+			 *	@param	vector2:入力ベクトル
+			 *	@return	外積結果
+			 */
+			static Vector3D CrossProduct(const Vector3D& vector1, const Vector3D& vector2);
+
+			// メソッド
+			// 零ベクトル
+			// 線形補間
+			// 各方向(↑↓→←)
+
+			float x; ///< x成分
+			float y; ///< y成分
+			float z; ///< z成分
+		};
+	}
+}
